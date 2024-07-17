1589e165e13e24996e6ede119f3d43c1c335c49f
Carol Fagundes <carolinarftk@gmail.com>
2024-07-16 22:49:43 -0300

    criando card

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,19 @@
+import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
+import Rodape from "componentes/Rodape";
+import Titulo from "componentes/Titulo";
 
 function Inicio() {
-  return <Cabecalho />;
+  return (
+    <>
+      <Cabecalho />
+      <Banner imagem={"home"} />
+      <Titulo>
+        <h1>Um lugar para guardar seus filmes e vídeos!</h1>
+      </Titulo>
+      <Rodape />
+    </>
+  );
 }
 
 export default Inicio;
